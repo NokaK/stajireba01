806459d1b6fd9f5a5e0d5f687ec45e0cc76b7520
NokaK <nona.kipiani07b@geolab.edu.ge>
2018-01-04 18:24:54 +0400

    commit
    add new commit

--- js/script.js
+++ js/script.js
@@ -12,3 +12,47 @@ $(document).ready(function(){
 // function closeNav() {
 //     document.getElementById("myNav").style.width = "0%";
 // }
+
+$(function(){
+
+        $('#nav-icon1').click(function() {
+          var clicks = $(this).data('clicks');
+          if (clicks) {
+            closeNav()
+          } else {
+             openNav()
+          }
+          $(this).data("clicks", !clicks);
+        });
+
+    function openNav() {
+        document.getElementById("myNav").style.width = "100%";
+    }
+
+    function closeNav() {
+        document.getElementById("myNav").style.width = "0%";
+    }
+
+        });
+
+// $(function(){
+
+//         $('#nav-icon1').click(function() {
+//           var clicks = $(this).data('clicks');
+//           if (clicks) {
+//             closeNav()
+//           } else {
+//              openNav()
+//           }
+//           $(this).data("clicks", !clicks);
+//         });
+
+//     function openNav() {
+//         document.getElementById("auto1").style.color = "red";
+//     }
+
+//     function closeNav() {
+//         document.getElementById("auto1").style.width = "0%";
+//     }
+
+//         });
